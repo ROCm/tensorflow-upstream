1401d5cad7c83caec5bfcec4f0dd41c7a3ede23b
root <root@banff-cyxtera-s70-4.ctr.dcgpu>
2024-07-17 09:20:57 +0000

    refactor

--- tensorflow/core/kernels/CK/gemm_layernorm_gemm_softmax_gemm/op.cu.cc
+++ tensorflow/core/kernels/CK/gemm_layernorm_gemm_softmax_gemm/op.cu.cc
@@ -7,9 +7,18 @@
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 namespace tensorflow {
 using GPUDevice = Eigen::GpuDevice;
-
 template <typename T>
 Status ComputeInternal(const GPUDevice& d, const void* mat_A0,
+                       const void* mat_B0, const void* mat_C,
+                       const void* mat_B1, const void* mat_B2,
+                       const void* Gamma, const void* Beta, const void* Keymask,
+                       void* mat_D, int K, int M, int N0, int N1, int long_seq,
+                       int N2, int B_kv, int head_num, float lrelu_alpha,
+                       bool do_layer_norm, bool do_leaky_relu,
+                       bool do_query_mask);
+
+template <>
+Status ComputeInternal<ck_tile::fp16_t>(const GPUDevice& d, const void* mat_A0,
                        const void* mat_B0, const void* mat_C,
                        const void* mat_B1, const void* mat_B2,
                        const void* Gamma, const void* Beta, const void* Keymask,
