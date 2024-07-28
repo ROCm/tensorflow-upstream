caf720bb540821c42a38452f19072ee91caa4d3d
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2024-07-28 02:28:10 +0000

    Use consistent compilation settings in build_whl.sh

--- build_whl.sh
+++ build_whl.sh
@@ -1,2 +1,2 @@
 bazel build -c opt --copt -g --strip=never --copt=-mavx --copt=-mavx2 --config=rocm --copt=-Wno-invalid-constexpr //tensorflow:libtensorflow_cc.so
-bazel build -c opt --copt -g --strip=never --copt=-mavx --copt=-mavx2 --config=rocm  //tensorflow:libtensorflow_framework.so
+bazel build -c opt --copt -g --strip=never --copt=-mavx --copt=-mavx2 --config=rocm --copt=-Wno-invalid-constexpr //tensorflow:libtensorflow_framework.so
