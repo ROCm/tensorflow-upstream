1114499ebfa6f24fd240926d5411b15da5dc195e
Jason Furmanek <Jason.Furmanek@amd.com>
2024-09-10 00:58:28 +0000

    Disable kAllNHWC

--- third_party/xla/xla/service/gpu/gpu_layout_assignment.cc
+++ third_party/xla/xla/service/gpu/gpu_layout_assignment.cc
@@ -129,9 +129,9 @@ HeuristicLayoutAssignment(const HloInstruction* instr,
     return kAllNHWC;
   }
 
-  const auto* rocm_compute_capability =
-      std::get_if<se::RocmComputeCapability>(&gpu_version);
-  if (rocm_compute_capability && input_ty == F16) return kAllNHWC;
+  //const auto* rocm_compute_capability =
+  //    std::get_if<se::RocmComputeCapability>(&gpu_version);
+  //if (rocm_compute_capability && input_ty == F16) return kAllNHWC;
 
   // If we're not Volta or not fp16/bfloat16, or not conv2D, the decision is
   // easy: Use NCHW.
