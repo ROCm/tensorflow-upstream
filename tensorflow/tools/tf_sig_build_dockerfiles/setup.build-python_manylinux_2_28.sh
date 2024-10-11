8f73a1cd6f6e02c5c6dc2b00d203980d3e3d9cd0
Jason Furmanek <Jason.Furmanek@amd.com>
2024-10-11 09:06:02 -0700

    Fix tf_keras

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.build-python_manylinux_2_28.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.build-python_manylinux_2_28.sh
@@ -91,3 +91,4 @@ python3 -m pip install --user virtualenv
 
 # Disable the cache dir to save image space, and install packages
 python3 -m pip install --no-cache-dir -r $REQUIREMENTS -U
+python3 -m pip install --no-cache-dir --no-deps tf-keras==2.17.0
