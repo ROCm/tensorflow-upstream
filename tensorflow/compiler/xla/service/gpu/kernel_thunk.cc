63e022fa2b13a4096ff919a8d700a151f00fed18
scxfjiang <xuefei.jiang@amd.com>
2025-01-21 22:51:46 +0800

    revert log

--- tensorflow/compiler/xla/service/gpu/kernel_thunk.cc
+++ tensorflow/compiler/xla/service/gpu/kernel_thunk.cc
@@ -107,8 +107,7 @@ Status KernelThunk::ExecuteOnStream(const ExecuteParams& params) {
     kernel = it->second.get();
   }
 
-  // LOG(INFO) << "Launching " << kernel->name();
-  // LOG(INFO) << "Launch Dimension " << launch_dimensions.ToString();
+  VLOG(3) << "Launching " << kernel->name();
   absl::InlinedVector<se::DeviceMemoryBase, 4> buffer_args;
   for (const BufferAllocation::Slice& arg : args_) {
     se::DeviceMemoryBase buf = params.buffer_allocations->GetDeviceAddress(arg);
