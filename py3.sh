56cd1a44cab04faa8455dafee527848509d14923
kithumma <kiran.thumma@amd.com>
2025-02-07 05:14:05 +0000

    update 6.3.

--- py3.sh
+++ py3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export ASAN_OPTIONS="detect_leaks=0,detect_odr_violation=0,allocator_may_return_null=1"
-export LD_PRELOAD="/opt/rocm-6.3.0.2/lib/llvm/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so /opt/rocm-6.3.0.2/lib/asan/libhsa-runtime64.so"
+export LD_PRELOAD="/opt/rocm-6.3.3/lib/llvm/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so /opt/rocm-6.3.3/lib/asan/libhsa-runtime64.so"
 export HSA_XNACK=1
 
 exec bash -c "/usr/bin/python3 ${*@Q}"
