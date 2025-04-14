aa686cfad94a1cef9dfa7243ab5a2ec05c84dc82
Chao Chen <cchen104@amd.com>
2025-04-14 19:04:22 +0800

    fixed build error

--- third_party/xla/xla/service/gpu/transforms/horizontal_input_fusion.cc
+++ third_party/xla/xla/service/gpu/transforms/horizontal_input_fusion.cc
@@ -109,19 +109,19 @@ std::vector<HloInstruction*> FindAndSortFusionCandidates(
 
   std::sort(fusion_instrs.begin(), fusion_instrs.end(),
             [&](const HloInstruction* a, const HloInstruction* b) {
-              // Shape shape_a = 
-              //     GetInputShapeForMultiOutputFusion(*a, device_info);
-              // Shape shape_b = 
-              //     GetInputShapeForMultiOutputFusion(*b, device_info);
-              auto tuple_for_op = [](const HloInstruction* op){
-              // if (!ShapeUtil::EqualIgnoringElementType(shape_a, shape_b)) {
+              Shape shape_a = 
+                  GetInputShapeForMultiOutputFusion(*a, device_info);
+              Shape shape_b = 
+                  GetInputShapeForMultiOutputFusion(*b, device_info);
+              auto tuple_for_op = [](const Shape& shape,
+                                    const HloInstruction* op){
                 // Sort shapes according to dimensions, so that the same input
                 // shapes will be placed adjacent each other.
                 // return CompareShapeDimsFromLeftToRight(shape_a, shape_b);
                 return std::tuple{shape.rank(), shape.dimensions(),
-                  GetInstrCountOfFusible(*op), op->unique_id()};                
+                        GetInstrCountOfFusible(*op), op->unique_id()};
               };
-              return tuple_for_op(a) < tuple_for_op(b);
+              return tuple_for_op(shape_a, a) < tuple_for_op(shape_b, b);
               // Sort `fusion_instrs` according to instruction counts, because
               // we'd like to fuse together computations of similar sizes.
               // return GetInstrCountOfFusible(*a) < GetInstrCountOfFusible(*b);
