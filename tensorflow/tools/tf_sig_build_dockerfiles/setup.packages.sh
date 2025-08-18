16ea9a6cfa817c96905491e68992d83b249e66de
JeniferC99 <150404595+JeniferC99@users.noreply.github.com>
2025-08-18 15:14:25 -0700

    Update setup.packages.sh

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.packages.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.packages.sh
@@ -26,7 +26,7 @@ else
 # Prevent apt install tzinfo from asking our location (assumes UTC)
 export DEBIAN_FRONTEND=noninteractive
 
-#apt-get update
+apt-get update
 # Remove commented lines and blank lines
 apt-get install -y --no-install-recommends $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' "$1" | sort -u)
 rm -rf /var/lib/apt/lists/*
