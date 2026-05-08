1eeaa427d603f629061409aeef794897c0fe122a
Harsha Havanur Shamsundara <hahavanu@amd.com>
2026-05-08 16:17:35 +0800

    alwayslink=True for kernel libs

--- tensorflow/core/util/cuda_sparse.h
+++ tensorflow/core/util/cuda_sparse.h
@@ -623,7 +623,7 @@ class GpuSparseMatrixDescriptor {
 #if GOOGLE_CUDA
     TF_RETURN_IF_GPUSPARSE_ERROR(cusparseCreateMatDescr(&descr_));
 #elif TENSORFLOW_USE_ROCM
-    TF_RETURN_IF_GPUSPARSE_ERROR(se::wrap::hipsparseCreateMatDescr(&descr_));
+    TF_RETURN_IF_GPUSPARSE_ERROR(hipsparseCreateMatDescr(&descr_));
 #endif
     initialized_ = true;
     return absl::OkStatus();
@@ -645,7 +645,7 @@ class GpuSparseMatrixDescriptor {
 #if GOOGLE_CUDA
       cusparseDestroyMatDescr(descr_);
 #elif TENSORFLOW_USE_ROCM
-      se::wrap::hipsparseDestroyMatDescr(descr_);
+      hipsparseDestroyMatDescr(descr_);
 #endif
       initialized_ = false;
     }
